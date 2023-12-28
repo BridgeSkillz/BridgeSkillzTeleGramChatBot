76fc4b2b635b3fbdd2d9f9567b9ba08fd68139cb
Devang S.P. Singh <devangshauryapratapsingh@gmail.com>
2023-12-28 12:04:28 +0530

    ✨ feat: typing indicator & commands in menu

--- bridge_skillz_gpt/tele_bot/BridgeSKillzBot.py
+++ bridge_skillz_gpt/tele_bot/BridgeSKillzBot.py
@@ -1,28 +1,28 @@
 from openai import OpenAI
-from telegram import Update
+from telegram import BotCommand, Update
 from bridge_skillz_gpt.tele_bot.BridgeSkillzBotBrain import BRAIN
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    Application,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    ContextTypes,
+)
+from telegram import constants
 from rich.console import Console
 from rich.text import Text
 import os
 from dotenv import load_dotenv
 import logging
-import logging
-from openai import OpenAI
-from telegram import Update
-from bridge_skillz_gpt.tele_bot.BridgeSkillzBotBrain import BRAIN
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-from rich.console import Console
-from rich.text import Text
-import os
-from dotenv import load_dotenv
+
 
 load_dotenv()
 
 https_logger = logging.getLogger("httpx")
-https_logger.disabled=True
+https_logger.disabled = True
 
-TOKEN = os.environ['client_tele_bot_token']
+TOKEN = os.environ["client_tele_bot_token"]
 console = Console()
 
 MODEL = OpenAI(base_url="http://localhost:8001/v1",
@@ -33,12 +33,13 @@ def getSystemPrompt():
     with open("bridge_skillz_gpt\\tele_bot\persona.txt", "r") as file:
         return file.read()
 
-def printPrompt(msg,color="red"):
+
+def printPrompt(msg, color="red"):
     console.print(Text(msg, style=color))
 
-SYSTEMPROMPT = [
-    {"role": "system", "content": getSystemPrompt()}
-]
+
+SYSTEMPROMPT = [{"role": "system", "content": getSystemPrompt()}]
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
@@ -59,9 +60,12 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
     printPrompt(f"({user.full_name}) Query    >>    {Query}")
     BRAIN.insertChatHistory(user.id, "user", Query)
-    
-    Query = "Reply in 10 Words : "+Query
+
+    Query = "Reply in 10 Words : " + Query
     ChatHistory = SYSTEMPROMPT + BRAIN.getChatHistoryByUserID(user.id)
+
+    await update.effective_chat.send_action(constants.ChatAction.TYPING)
+
     Response = ""
     while not Response:
         Response = MODEL.chat.completions.create(
@@ -70,16 +74,32 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             temperature=0.7,
         )
         Response = Response.choices[0].message.content
-        Response=Response.replace("[INST"," ").replace("[/INST]"," ").replace("[/INST"," ")
-    printPrompt(f"({user.full_name}) Response >>    {Response}","blue")
-    BRAIN.insertChatHistory(user.id, 'assistant', Response)
+        Response = (
+            Response.replace("[INST", " ")
+            .replace("[/INST]", " ")
+            .replace("[/INST", " ")
+        )
+    printPrompt(f"({user.full_name}) Response >>    {Response}", "blue")
+    BRAIN.insertChatHistory(user.id, "assistant", Response)
     print("\n\n")
     await update.message.reply_text(Response)
 
 
+async def post_init(application: Application):
+    """sets the bot commands in the telegram bot, these commands shows in hamburger menu"""
+
+    command_description = {"start": "Start your conversation with Acharya"}
+
+    commands = [
+        BotCommand(command="start", description=command_description["start"]),
+        # BotCommand(command='help', description=command_description['help']),
+    ]
+    await application.bot.set_my_commands(commands)
+
+
 def main():
     # Create the Application and pass it your bot's token.
-    app = ApplicationBuilder().token(TOKEN).build()
+    app = ApplicationBuilder().token(TOKEN).post_init(post_init).build()
 
     # Add handlers
     app.add_handler(CommandHandler("start", start))
@@ -89,8 +109,10 @@ def main():
     # Run the bot until the user presses Ctrl-C
     app.run_polling()
 
+
 def start_bot():
     main()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
